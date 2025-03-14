917d3515bf7ad84016e32617f4ec0b5b47cfe41a
Martin Hobert <info@dosh.dk>
2025-03-14 23:32:40 +0100

    add image optimize

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,11 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
+    image: {
+        remotePatterns: [{ protocol: "https" }],
+    },
+    vite: {
       plugins: [tailwindcss()],
-  },
-
-  integrations: [react()],
+    },
+    integrations: [react()],
 });
