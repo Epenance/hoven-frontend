0d700367fab07356713b11ad964cb94c18bccaac
Martin Hobert <info@dosh.dk>
2025-07-15 15:19:38 +0200

    Add tailwind config, and adjust text sizes

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+        './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}',
+    ],
+    safelist: [
+        'list-disc',
+        'mb-2',
+        'my-4',
+        'last:mb-0',
+        'pl-6'
+        // Add other CMS classes you use
+    ],
+    // ... rest of config
+}
