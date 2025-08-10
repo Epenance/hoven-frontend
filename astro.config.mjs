8e6592db74a63f906b88dbaf80de3f0bcd10fead
Martin Hobert <martin.hobert@lego.com>
2025-08-10 21:04:54 +0200

    Add site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import { loadEnv } from "vite";
 
 import react from "@astrojs/react";
 
+const {SITE_URL, CMS_DOMAIN} = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+
+console.log('SITE_URL', SITE_URL)
+console.log('CMS_DOMAIN', CMS_DOMAIN)
+
 // https://astro.build/config
 export default defineConfig({
+    site: SITE_URL ? SITE_URL : undefined,
     image: {
-        domains: ['localhost', 'hoven-cms-production.up.railway.app'],
+        domains: ['hoven-cms-production.up.railway.app', CMS_DOMAIN],
     },
     vite: {
       plugins: [tailwindcss()],
